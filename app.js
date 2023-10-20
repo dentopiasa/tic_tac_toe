7b399ea0702fece14b7d91f8fb1d7992aa6e7683
dentopiasa <castelli_philippe@hotmail.com>
2023-10-20 19:25:01 +0300

    progress function

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const startBtn = document.querySelector("#play");
 
+const Gameboard = (function() {
+    let gameboard = ["", "", "", "", "", "", "", "", ""];
+
+    const render = function() {
+        let boardHTML = "";
+        gameboard.forEach(function(square, index){
+            boardHTML += `<div class="square" class="square"-${index}>${square}</div>`
+        })
+    }
+}
+)();
+
 startBtn.addEventListener("click", ()=> {
     //Game.start()
 })
