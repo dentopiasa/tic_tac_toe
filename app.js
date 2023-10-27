5ee4efff01ebbfa0e0c2eba9663ee2bd5e378587
dentopiasa <castelli_philippe@hotmail.com>
2023-10-27 20:55:48 +0300

    win and tie functions

--- app.js
+++ app.js
@@ -62,6 +62,14 @@ const Game = (() => { //game controls and logic
 
         Gameboard.update(index, players[currentPlayerIndex].mark)
 
+        if(checkForWin(Gameboard.getGameboard(), players[currentPlayerIndex.mark])) {
+            gameOver = true;
+            alert("bla");
+        } else if (checkForTie(Gameboard.getGameboard())) {
+            gameOver = true;
+            alert("tie");
+        }
+
         currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
     }
 
@@ -83,6 +91,30 @@ const startBtn = document.querySelector("#play");
 startBtn.addEventListener("click", ()=> {
     Game.start();
 })
+
+function checkForWin(board) {
+    const winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+    for(i = 0; i < winningCombinations.length; i++) {
+        const [a, b, c] = winningCombinations[i];
+        if (board[a] && board[a] === board[b] && board [a] === board[c]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function checkForTie(board) {
+    return board.every(cell => cell !=="")
+}
 
 const restartBtn = document.querySelector("#restart");
 restartBtn.addEventListener("click", () => {
